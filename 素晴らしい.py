4e029de36b4cae33a7f1918d24ce9afdbd2d3af5
Ennuilw <104221612+Ennuilw@users.noreply.github.com>
2022-07-04 16:57:22 +0900

    Update 素晴らしい.py

--- 素晴らしい.py
+++ 素晴らしい.py
@@ -1,12 +1,13 @@
 import discord, random
 from discord.utils import get
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 client = discord.Client(intents=intents)
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.author == client.user:return
     role = discord.utils.get(message.guild.roles, id=991009276946419714)
     if not role in message.author.roles:return
     words=["素晴らしい", "素晴らしいな", "素晴らしい:clap:", "素晴らしいな:clap:"]
@@ -18,4 +19,5 @@ async def on_message(message):
         await message.channel.send(random.choice(words))
     elif ("すばらしい") in message.content:
         await message.channel.send(random.choice(words))
-client.run("TOKEN")
+
+client.run()   
